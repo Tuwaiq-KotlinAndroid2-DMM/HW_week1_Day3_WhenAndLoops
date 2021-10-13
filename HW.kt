d2051b6a573cd1267fde88c1ce3f8a4518028607
ghzlan <ghzlan-1996-a@hotmail.com>
2021-10-13 08:07:13 +0300

    My Hw day3

---
+++ HW.kt
@@ -0,0 +1,25 @@
+fun main(){
+    println("please enter month number")
+
+  var month = Integer.valueOf(readLine())
+
+         when(month){
+       1-> println("jun has 31 day")
+       2-> println("Feb has 28 day")
+       3-> println("Mar has 31 day")
+       4-> println("Apr has 30 day")
+       5-> println("may has 31 day")
+       6-> println("jun has 30 day")
+        7-> println("jul has 31 day")
+       8-> println("aug has 31 day")
+       9-> println("Sep has 30 day")
+       10-> println("Oct has 31 day")
+      11-> println("Nov has 30 day")
+            12-> println("Dec has 31 day")
+            else-> println("Invalid month")
+            }
+println("***********")
+
+
+
+         }
---
+++ Hw3.kt
@@ -0,0 +1,22 @@
+fun  main(){
+
+    var x = arrayOf(28,4,6,5,7,8,9,)
+    var on =0
+    for (i in x) {
+        if (i % 2 !=0)
+         on++
+
+    }
+    println(x.contentToString())
+    println("there are $on odds numbers")
+
+
+
+
+
+
+
+
+
+
+}
